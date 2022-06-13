2e0e514a43c4c812bb4e48ca5b2354ccbce24138
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-13 13:02:50 +0200

    bootstrap server

---
+++ server/src/main.ts
@@ -0,0 +1,6 @@
+async function main() {
+  console.log('hello world');
+  // start our server  
+}
+
+main();
---
+++ server/src/utils/createServer.ts
@@ -0,0 +1,4 @@
+export async function createServer() {
+  // need: root query
+  // root query needs root resolver
+}
