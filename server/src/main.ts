6b641325a2bdd12ec538c5ca2f870b00bdfee2b7
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-13 14:47:35 +0200

    Get gql server running

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,24 @@
+import 'reflect-metadata';
+import { createServer } from './utils/createServer';
+
 async function main() {
-  console.log('hello world');
-  // start our server  
+  // start our server
+  const { app, server } = await createServer();
+
+  app.get('/healthcheck', async () => 'ok');
+
+  await server.start();
+  
+  app.register(
+    server.createHandler({
+      cors: false,
+    })
+  );
+
+  await app.listen({
+    port: 4000,
+  });
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
 main();
--- server/src/utils/createServer.ts
+++ server/src/utils/createServer.ts
@@ -1,4 +1,29 @@
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-fastify';
+import fastify from 'fastify';
+import { buildSchema } from 'type-graphql'
+import UserResolver from '../modules/user/user.resolver';
+
+const app = fastify();
+
+function buildContext() {}
+
 export async function createServer() {
   // need: root query
   // root query needs root resolver
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+  });
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      // Graceful shutdown
+      // https://www.apollographql.com/docs/apollo-server/api/plugin/drain-http-server/
+      ApolloServerPluginDrainHttpServer({ httpServer: app.server }),
+    ],
+    context: buildContext,
+  });
+
+  return { app, server };
 }
